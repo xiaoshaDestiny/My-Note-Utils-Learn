048df81842ef6a50b0d021cda4137b0828c4fd68
xiaoshaDestiny <2284420047@qq.com>
2019-12-27 22:59:29 +0800

    日期工具类

---
+++ src/main/java/com/learn/mySource/DateSource.java
@@ -0,0 +1,27 @@
+package com.learn.mySource;
+
+import java.util.Calendar;
+
+/**
+ * @author xrb
+ * @create 2019-12-27 16:38
+ * 日期操作相关的资源
+ */
+public class DateSource {
+
+    /**
+     * 获得某个月最大天数
+     * @param yearMonth 可以是8位字符串 yyyyMMdd  也可以是6位字符串yyyyMM
+     * @return
+     */
+    public int getMaxDayByYearMonth(String yearMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DATE, 1);
+        calendar.set(Calendar.YEAR, Integer.parseInt( yearMonth.substring(0,4)));
+        calendar.set(Calendar.MONTH, Integer.parseInt(yearMonth.substring(5,6)) - 1);
+        return calendar.getActualMaximum(Calendar.DATE);
+    }
+
+
+
+}
