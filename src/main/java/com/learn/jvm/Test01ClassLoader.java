4f6a4463bf085aa98e993b1ac2a4b1818ddef362
xiaoshaDestiny <2284420047@qq.com>
2019-12-30 23:25:48 +0800

    类加载器

--- src/main/java/com/learn/jvm/Test01ClassLoader.java
+++ src/main/java/com/learn/jvm/Test01ClassLoader.java
@@ -18,6 +18,8 @@ public class Test01ClassLoader {
         //3、应用程序类加载器 APP  Java也叫系统类加载器，加载当前应用的classpath的所有类
         Test01ClassLoader t1 = new Test01ClassLoader();
         System.out.println("Test01ClassLoader : "+t1.getClass().getClassLoader());
+        System.out.println("Test01ClassLoader Parent : "+t1.getClass().getClassLoader().getParent());
+        System.out.println("Test01ClassLoader Parent Parent: "+t1.getClass().getClassLoader().getParent().getParent());
 
     }
 }
