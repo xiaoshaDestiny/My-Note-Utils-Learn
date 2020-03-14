9abc62ea5f89d00383b54756ffc0ba7935d9dee9
xiaoshaDestiny <2284420047@qq.com>
2020-03-14 09:19:39 +0800

    整合activemq

---
+++ learn-activemq/src/main/java/com/learn/activemq/queue/JmsProdece.java
@@ -0,0 +1,18 @@
+package com.learn.activemq.queue;
+
+import org.apache.activemq.spring.ActiveMQConnectionFactory;
+
+/**
+ * @author xrb
+ * @create 2020-03-13 21:17
+ */
+public class JmsProdece {
+    public static void main(String[] args) {
+
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+
+
+
+
+    }
+}
