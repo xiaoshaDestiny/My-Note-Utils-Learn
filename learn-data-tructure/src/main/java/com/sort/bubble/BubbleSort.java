f560445bda014cb701283bf712e126b908cf4f6b
xiaoshaDestiny <2284420047@qq.com>
2020-04-14 20:10:33 +0800

    冒泡排序

--- learn-data-tructure/src/main/java/com/sort/bubble/BubbleSort.java
+++ learn-data-tructure/src/main/java/com/sort/bubble/BubbleSort.java
@@ -8,32 +8,38 @@ import java.util.Arrays;
  * 冒泡排序
  *
  * 一共进行数组长度-1次的大循环
- *
- *
  * 每一趟找出 最大 的值确定
- *
- *
- *
  */
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {4,-1,5,3};
+        int[] arr = {-1,-4,12,13};
         System.out.println(Arrays.toString(arr));
 
+        //总共循环数组长度减一次
+        int temp = 0;
+        boolean flag = false;
         for (int j = 0; j < arr.length - 1 ; j++) {
-            //总共循环数组长度减一次
-            int temp = 0;
+
             for (int i = 0; i < arr.length - 1 - j; i++) {
                 //第一趟的比较次数 是数组长度-1次
                 //第二趟          是数组长度-2次
-
                 if(arr[i] > arr [i+1]){
+                    flag = true;
                     temp = arr[i];
                     arr[i] = arr[i+1];
                     arr[i+1] = temp;
                 }
             }
+
+            if(!flag){
+                break;
+            }else {
+                flag = false;
+            }
+            System.out.println(Arrays.toString(arr));
         }
-        System.out.println(Arrays.toString(arr));
     }
+
+
+
 }
