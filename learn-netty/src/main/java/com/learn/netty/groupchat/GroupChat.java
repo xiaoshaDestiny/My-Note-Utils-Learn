2df7a21b6785ea749124e5a7f4bb4ead4f382fe1
xiaoshaDestiny <2284420047@qq.com>
2020-05-23 18:52:30 +0800

    群聊服务启动类

---
+++ learn-netty/src/main/java/com/learn/netty/groupchat/GroupChat.java
@@ -0,0 +1,69 @@
+package com.learn.netty.groupchat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+/**
+ * @author xu.rb
+ * @since 2020-05-22 16:53
+ */
+public class GroupChat {
+
+    private int port;//端口
+
+    public GroupChat(int port) {
+        this.port = port;
+    }
+
+    //run方法 处理客户端请求
+    public void run() throws InterruptedException {
+        //线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup(8);
+
+        try {
+            //启动引导
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,128)
+                    .childOption(ChannelOption.SO_KEEPALIVE,true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+
+                            //编码解码器
+                            pipeline.addLast("decoder",new StringDecoder());
+                            pipeline.addLast("encoder",new StringEncoder());
+
+                            //加入自己的业务处理Handler
+                            pipeline.addLast(null);
+                        }
+                    });
+            System.out.println("netty服务器启动。。");
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+
+            //监听关闭时间
+            channelFuture.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new GroupChat(7000).run();
+    }
+}
