a79673838c77601df686b1f8c39f36ad78c4b0f6
xiaoshaDestiny <2284420047@qq.com>
2020-06-12 16:17:55 +0800

    笔试准备

--- learn-sword-offer/src/test/java/com/test/huawei/Main.java
+++ learn-sword-offer/src/test/java/com/test/huawei/Main.java
@@ -1,83 +1,9 @@
 package com.test.huawei;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-class PrintString{
-    private int num = 1;
-
-    public synchronized void printAString(){
-        while (num%4 != 1){
-            try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        num++;
-        System.out.print("A");
-        notifyAll();
-    }
-
-    public synchronized void printBString(){
-        while (num%4 != 2){
-            try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        num++;
-        System.out.print("B");
-        notifyAll();
-    }
-
-    public synchronized void printCString(){
-        while (num%4 != 3){
-            try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        num++;
-        System.out.print("C");
-        notifyAll();
-    }
-
-    public synchronized void printDString(){
-        while (num%4 != 0){
-            try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-        num++;
-        System.out.print("D");
-        notifyAll();
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()){
-            int num = new Integer(scanner.nextLine());
 
-            PrintString p = new PrintString();
-            new Thread(()->{
-                for (int i = 0; i < num; i++) {
-                    p.printAString();
-                }
-            }).start();
-
-            new Thread(()->{
-                for (int i = 0; i < num; i++) {
-                    p.printBString();
-                }
-            }).start();
-
-            new Thread(()->{
-                for (int i = 0; i < num; i++) {
-                    p.printCString();
-                }
-            }).start();
-
-            new Thread(()->{
-                for (int i = 0; i < num; i++) {
-                    p.printDString();
-                }
-                System.out.println();
-            }).start();
-        }
 
     }
 }
--- learn-sword-offer/src/test/java/com/test/huawei/Store.java
+++ learn-sword-offer/src/test/java/com/test/huawei/Store.java
@@ -15,6 +15,145 @@ import java.util.stream.Collectors;
 public class Store {
 
 
+
+    /**
+     * 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+     * 注意每个输入文件有多组输入，即多个字符串用回车隔开
+     * 输入描述:
+     * 字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
+     *
+     * 输出描述:
+     * 删除字符串中出现次数最少的字符后的字符串。
+     *
+     * 输入
+     * abcdd
+     *
+     * 输出
+     * dd
+     */
+    public void test14(){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            char[] lines = line.toCharArray();
+
+            Map<Character, Integer> map = new HashMap<>();
+            for (int i = 0; i <lines.length ; i++) {
+                int cal = 0;
+                for (int j = 0; j <lines.length; j++) {
+                    if(lines[i] == lines[j]){
+                        cal ++;
+                    }
+                }
+                map.put(lines[i],cal);
+            }
+            Set<Character> set = map.keySet();
+
+            int min = 20;
+            for (Character ch :set) {
+                if(map.get(ch) < min){
+                    min = map.get(ch);
+                }
+            }
+
+            List<Character> list = new ArrayList<>();
+            for (Character ch :set) {
+                if(map.get(ch) == min){
+                    list.add(ch);
+                }
+            }
+            for (int i = 0; i < list.size(); i++) {
+                line = line.replaceAll(list.get(i).toString(), "");
+            }
+            System.out.println(line);
+        }
+    }
+
+
+    public void test13(){
+
+        /*class PrintString{
+            private int num = 1;
+
+            public synchronized void printAString(){
+                while (num%4 != 1){
+                    try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+                num++;
+                System.out.print("A");
+                notifyAll();
+            }
+
+            public synchronized void printBString(){
+                while (num%4 != 2){
+                    try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+                num++;
+                System.out.print("B");
+                notifyAll();
+            }
+
+            public synchronized void printCString(){
+                while (num%4 != 3){
+                    try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+                num++;
+                System.out.print("C");
+                notifyAll();
+            }
+
+            public synchronized void printDString(){
+                while (num%4 != 0){
+                    try { wait(); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+                num++;
+                System.out.print("D");
+                notifyAll();
+            }
+        }
+
+
+        public class Main {
+            public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext()){
+                    int num = new Integer(scanner.nextLine());
+
+                    PrintString p = new PrintString();
+                    new Thread(()->{
+                        for (int i = 0; i < num; i++) {
+                            p.printAString();
+                        }
+                    }).start();
+
+                    new Thread(()->{
+                        for (int i = 0; i < num; i++) {
+                            p.printBString();
+                        }
+                    }).start();
+
+                    new Thread(()->{
+                        for (int i = 0; i < num; i++) {
+                            p.printCString();
+                        }
+                    }).start();
+
+                    new Thread(()->{
+                        for (int i = 0; i < num; i++) {
+                            p.printDString();
+                        }
+                        System.out.println();
+                    }).start();
+                }
+
+            }
+        }
+*/
+    }
+
+
+
     @Test
     public void test12(){
 
