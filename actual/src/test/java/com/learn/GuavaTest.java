37221df1c4a8c25ee0ba63d153d04b44cd2ee8cf
xiaoshaDestiny <2284420047@qq.com>
2021-01-03 01:22:59 +0800

    test(actual):guava pom

---
+++ actual/src/test/java/com/learn/GuavaTest.java
@@ -0,0 +1,25 @@
+package com.learn;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author xu.rb
+ * @since 2020-12-31 00:00
+ */
+public class GuavaTest {
+
+    @Test
+    public void testGuava(){
+        List<String> strings = Lists.newArrayList("");
+        HashMap<Object, Object> objectObjectHashMap = Maps.newHashMap();
+
+
+    }
+}
